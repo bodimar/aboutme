1cbc2a85adfcfce3ff48057e3519ee761bffc709
Marko Bodi <bodimarko@outlook.com>
2016-05-26 20:45:09 -0700

    complete

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 var qOne = prompt('Is Marko proficient in Latin?').toLowerCase();
-if (qOne === 'no'|| 'n') {
+if (qOne === 'no'|| qOne ==='n') {
   alert('Correct!');
 } else {
-  alert('Game Over!');
+  alert('Incorrect!');
 }
 var qTwo = prompt('Does Marko have a High School Diploma?').toLowerCase();
-if (qTwo === 'yes' || 'y') {
+if (qTwo === 'yes' || qTwo ==='y') {
   alert('Correct!');
 } else {
-  alert('Game Over!')
+  alert('Incorrect!')
+}
+var qThree = prompt('Was Marko employed with Firm in 2014?').toLowerCase();
+if (qThree === 'no' || qThree ==='n') {
+  alert('Correct!');
+} else {
+  alert('Incorrect!');
+}
+var qFour = prompt('Does Mr. Bodi have the letter \'c\' in his first name?').toLowerCase();
+if (qFour === 'no' || qFour ==='n') {
+  alert('Correct!');
+} else {
+  alert('Incorrect!');
 }
