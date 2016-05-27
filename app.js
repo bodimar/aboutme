11e8ba3fef6762e3087262b4ee4e383c81a08612
Marko Bodi <bodimarko@outlook.com>
2016-05-26 20:23:17 -0700

    project

---
+++ app.js
@@ -0,0 +1,12 @@
+var qOne = prompt('Is Marko proficient in Latin?').toLowerCase();
+if (qOne === 'no'|| 'n') {
+  alert('Correct!');
+} else {
+  alert('Game Over!');
+}
+var qTwo = prompt('Does Marko have a High School Diploma?').toLowerCase();
+if (qTwo === 'yes' || 'y') {
+  alert('Correct!');
+} else {
+  alert('Game Over!')
+}
